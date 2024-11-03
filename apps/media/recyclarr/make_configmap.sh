dd8e33865d312e004ae9a857f32b81595985a3c7
Patrick McDonagh <patrickjmcd@gmail.com>
2024-11-03 15:59:01 -0600

    fix: recyclarr config path

--- apps/media/recyclarr/make_configmap.sh
+++ apps/media/recyclarr/make_configmap.sh
@@ -1,3 +1,3 @@
-envsubst < config.template.yaml > config.yaml
-kubectl create configmap recyclarr-config --from-file=config.yaml -n media
-rm config.yaml
+envsubst < config.template.yaml > recyclarr.yaml
+kubectl create configmap recyclarr-config --from-file=recyclarr.yaml -n media
+rm recyclarr.yaml
