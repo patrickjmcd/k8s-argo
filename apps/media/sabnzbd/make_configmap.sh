9bc310c6c809970d3a0a9ff90e68926b461b4dc4
Patrick McDonagh <patrickjmcd@gmail.com>
2025-12-01 12:28:47 -0600

    fix: storage

--- apps/media/sabnzbd/make_configmap.sh
+++ apps/media/sabnzbd/make_configmap.sh
@@ -1,3 +1,4 @@
 envsubst < sabnzbd.template.ini > sabnzbd.ini
-kubectl replace configmap sabnzbd-config --from-file=sabnzbd.ini -n media
+kubectl delete configmap sabnzbd-config -n media --ignore-not-found
+kubectl create configmap sabnzbd-config --from-file=sabnzbd.ini -n media
 rm sabnzbd.ini
