498743ea3564806ce1a1c86f8551976ac4f91f15
Patrick McDonagh <patrickjmcd@gmail.com>
2025-12-30 11:34:18 -0600

    fix: mealie

--- apps/mealie/make_configmap.sh
+++ apps/mealie/make_configmap.sh
@@ -1,4 +1,4 @@
 envsubst < .env.template > .env
 kubectl delete configmap mealie-config --ignore-not-found
-kubectl create configmap mealie-config --from-file=.env
+kubectl create configmap mealie-config --from-env-file=.env
 rm .env
