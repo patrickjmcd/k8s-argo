896d9265d92111fd76c42baecdb18259513cb125
Patrick McDonagh <patrickjmcd@gmail.com>
2026-01-05 12:24:13 -0600

    feat: homarr

---
+++ apps/homarr-assets/secrets.sh
@@ -0,0 +1,9 @@
+kubectl delete secret db-encryption -n homarr --ignore-not-found
+kubectl create secret generic db-encryption \
+--from-literal=db-encryption-key="$HOMARR_ENCRYPTION_KEY" \
+--namespace homarr
+
+kubectl delete secret db-secret -n homarr --ignore-not-found
+kubectl create secret generic db-secret \
+--from-literal=db-url="postgresql://homarr:$HOMARR_DB_PASSWORD@$POSTGRESS_ADDRESS:5432/homarrdb" \
+--namespace homarr
