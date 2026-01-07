fa65b33d5df4dbd2ccc8ad710e21a3fbe3318ec6
Patrick McDonagh <patrickjmcd@gmail.com>
2026-01-07 08:51:10 -0600

    fix: pocketid

--- core/pocketid/make-configmap.sh
+++ core/pocketid/make-configmap.sh
@@ -1,4 +1,4 @@
 envsubst < .env.template > .env
-kubectl delete configmap pocketid-config --ignore-not-found
-kubectl create configmap pocketid --from-env-file=.env
+kubectl delete configmap -n pocketid pocketid-config --ignore-not-found
+kubectl create configmap -n pocketid pocketid-config --from-env-file=.env
 rm .env
