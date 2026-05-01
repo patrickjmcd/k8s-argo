08cde5a351025ca2bb3727cb64c764bbc4df14e2
Patrick McDonagh <patrickjmcd@gmail.com>
2026-05-01 11:57:02 -0500

    feat: configure pocketid to use 1Password secret for sensitive data

--- core/pocketid/make-configmap.sh
+++
@@ -1,4 +0,0 @@
-envsubst < .env.template > .env
-kubectl delete configmap -n pocketid pocketid-config --ignore-not-found
-kubectl create configmap -n pocketid pocketid-config --from-env-file=.env
-rm .env
